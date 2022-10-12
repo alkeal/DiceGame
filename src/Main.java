f7b2965c755967013231d899d08d1d8237fc3592
a <yhma22.alkeal@folkuniversitetet.nu>
2022-10-12 14:15:53 +0200

    1. försök att skapa en tärning

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-
+        //Tärningen skall ha två variabler: nuvarande värde
+        // och antal sidor (dvs max-värde för tärningen)
 
 
 
